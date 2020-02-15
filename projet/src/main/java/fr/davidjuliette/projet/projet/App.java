8c1e9cea4eb18545a75bd58b959a5630ff36e967
David91298 <icedavid912@gmail.com>
2020-02-15 21:17:51 +0100

    Initialisation du projet

---
+++ projet/src/main/java/fr/davidjuliette/projet/projet/App.java
@@ -0,0 +1,13 @@
+package fr.davidjuliette.projet.projet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
